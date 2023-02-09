be6568a7376e26af93a9e694dcf9c86afcc5aa30
Aristides-01 <aristidesngongo@gmail.com>
2023-02-09 16:48:14 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { RepositoryList } from './components/RepositoryList'
+
+import './styles/global.scss'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <RepositoryList />
+    </div>
+  )
+}
+
+export default App
